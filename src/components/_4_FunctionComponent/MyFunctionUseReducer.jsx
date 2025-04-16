e6adccacabcb398d0ad2c7a0bb4360e28500595d
sunqfuu <sagir.o@outlook.com>
2025-04-16 14:18:12 +0300

    Add MyFunctionUseReducer component and integrate it into the main render

---
+++ src/components/_4_FunctionComponent/MyFunctionUseReducer.jsx
@@ -0,0 +1,35 @@
+//
+import React, { useReducer } from "react";
+
+
+//Reducer
+const reducerSunqfu = (state, action) => {
+    switch (action.type) {
+        case "INCREMENT":
+            return { count: state.count + 1 };
+        
+        case "DECREMENT":
+            return { count: state.count - 1 };
+        
+        default:
+            return state;
+        
+    }
+}
+
+//Component
+
+const ReducerCounterSunqfu = () => {
+    //UseReducer
+    const [state, dispatch] = useReducer(reducerSunqfu, { count: 0 });
+
+    return (
+        <div>
+            <p>Count:{state.count}</p>
+            <button onClick={() => dispatch({ type: "INCREMENT" })} className="btn btn-primary ms-2 me-2">Artır</button>
+            <button onClick={() => dispatch({ type: "DECREMENT" })} className="btn btn-danger ">Azalt</button>
+        </div>
+    )
+}
+
+export default ReducerCounterSunqfu;
--- src/index.js
+++ src/index.js
@@ -8,13 +8,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 // Components
-// import App from './components/_1_Style/Style';
-import JsxTutorials from './components/_2_Jsx/JsxTutorials';
 import reportWebVitals from './reportWebVitals';
-import MyClassComponent from './components/_3_ClassComponents/MyClassComponent';
-import MyFunctionComponent from './components/_4_FunctionComponent/MyFunctionComponent';
-import MyFunctionSunqfuCounter from './components/_4_FunctionComponent/MyFunctionSunqfuCounter';
-import MyFunctionUseEffect from './components/_4_FunctionComponent/MyFunctionUseEffect';
+// import App from './components/_1_Style/Style';
+// import JsxTutorials from './components/_2_Jsx/JsxTutorials';
+// import MyClassComponent from './components/_3_ClassComponents/MyClassComponent';
+// import MyFunctionComponent from './components/_4_FunctionComponent/MyFunctionComponent';
+// import MyFunctionSunqfuCounter from './components/_4_FunctionComponent/MyFunctionSunqfuCounter';
+// import MyFunctionUseEffect from './components/_4_FunctionComponent/MyFunctionUseEffect';
+import MyFunctionUseReducer from './components/_4_FunctionComponent/MyFunctionUseReducer';
 
 
 //Root
@@ -26,7 +27,8 @@ root.render(
     {/* <MyClassComponent companyName="Sunqfu" /> */}
     {/* <MyFunctionComponent companyName= "Sunqfu" content="Function Component Data"/> */}
     {/* <MyFunctionSunqfuCounter />  */}
-    <MyFunctionUseEffect/> 
+    {/* <MyFunctionUseEffect/> */}
+    <MyFunctionUseReducer/>
     
   </React.StrictMode>
 );
